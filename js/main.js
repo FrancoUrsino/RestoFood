d0d7abe81820121d2e175677f1adb6839cfaeb35
Franco Ursino <franncoursinno@gmail.com>
2023-04-19 14:45:26 -0300

    start hero section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 const preloader = document.querySelector("[data-preload]");
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+const header = document.querySelector("[data-header]");
 
 //PRELOAD FUNCTION
 window.addEventListener("load", function () {
@@ -7,9 +11,7 @@ window.addEventListener("load", function () {
 });
 
 
-const navbar = document.querySelector("[data-navbar]");
-const navTogglers = document.querySelectorAll("[data-nav-toggler]");
-const overlay = document.querySelector("[data-overlay]");
+// NAVBAR FUNCTIONS
 
 const addEventOnElements = function (elements, eventType, callback) {
   for (let i = 0, len = elements.length; i < len; i++) {
@@ -17,7 +19,6 @@ const addEventOnElements = function (elements, eventType, callback) {
   }
 }
 
-// NAVBAR FUNCTIONS
 const togglerNav = function () {
   navbar.classList.toggle("active");
   overlay.classList.toggle("active");
@@ -25,3 +26,27 @@ const togglerNav = function () {
 }
 
 addEventOnElements(navTogglers, "click", togglerNav)
+
+
+// HEADER FUNCTIONS
+
+let lastScroll = 0;
+const hideHeader = function (){
+  const isScrollBtn = lastScroll < window.scrollY;
+  if (isScrollBtn){
+    header.classList.add("hide");
+  } else{
+    header.classList.remove("hide")
+  }
+
+  lastScroll = window.scrollY;
+  hideHeader();
+}
+
+window.addEventListener("scroll", function (){
+if (window.scrollY >= 50){
+  header.classList.add("active");
+} else{
+  header.classList.remove("active");
+}
+})
